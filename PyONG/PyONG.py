a34ff77ea4d18239d8ed0371e1143236477ce3a9
KontrollFreek <72424960+KontrollFreek@users.noreply.github.com>
2021-11-20 11:33:19 -0500

    added a menu (bugged

--- PyONG/PyONG.py
+++ PyONG/PyONG.py
@@ -9,7 +9,36 @@ win.bgcolor('black')
 win.setup(width=1000, height=600)
 win.tracer(0)
 
-# Score
+# Menu
+menu1 = turtle.Turtle()
+menu1.speed(0)
+menu1.color('black')
+menu1.penup()
+menu1.hideturtle()
+menu1.goto(0, 180)
+menu1.write('PyONG', align='center', font=('Courier', 90, 'bold'))
+
+menu2 = turtle.Turtle()
+menu2.speed(0)
+menu2.color('black')
+menu2.penup()
+menu2.hideturtle()
+menu2.goto(0, -90)
+menu2.write('Press Space to start', align='center', font=('Courier', 45, 'bold'))
+
+while True:
+    time.sleep(2)
+    menu1.clear()
+    menu2.clear()
+    menu1.color('white')
+    menu2.color('white')
+    menu1.write('PyONG', align='center', font=('Courier', 90, 'bold'))
+    menu2.write('Press Space to start', align='center', font=('Courier', 45, 'bold'))
+    break
+
+
+# Varables
+p = True
 score1 = 0
 score2 = 0
 
@@ -18,7 +47,7 @@ pad1 = turtle.Turtle()
 pad1.speed(0)
 pad1.shape('square')
 pad1.shapesize(stretch_wid=5, stretch_len=1)
-pad1.color('white')
+pad1.color('black')
 pad1.penup()
 pad1.goto(-400, 0)
 
@@ -27,7 +56,7 @@ pad2 = turtle.Turtle()
 pad2.speed(0)
 pad2.shape('square')
 pad2.shapesize(stretch_wid=5, stretch_len=1)
-pad2.color('white')
+pad2.color('black')
 pad2.penup()
 pad2.goto(400, 0)
 
@@ -35,20 +64,20 @@ pad2.goto(400, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape('square')
-ball.color('white')
+ball.color('black')
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.14
-ball.dy = 0.14
+ball.dx = 0
+ball.dy = 0
 
 # Pen
-pen = turtle.Turtle()
-pen.speed(0)
-pen.color('white')
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write('Player 1: 0  Player 2: 0', align='center', font=('Courier', 24, 'bold'))
+score = turtle.Turtle()
+score.speed(0)
+score.color('black')
+score.penup()
+score.hideturtle()
+score.goto(0, 160)
+score.write('Player 1: 0  Player 2: 0', align='center', font=('Courier', 24, 'bold'))
 
 # Paddle 1 Movement
 def pad1_up():
@@ -72,7 +101,36 @@ def pad2_dwn():
     y -= 20
     pad2.sety(y)
 
+# Game start
+def gamestart():
+    if (p = False):
+        pad1.sety(0)
+        pad1.color('white')
+
+        pad2.sety(0)
+        pad2.color('white')
+
+        ball.dx = 0.14
+        ball.dy = 0.14
+        ball.color('white')
+
+        score.clear()
+        score.color('white')
+        score.goto(0,260)
+        score.write('Player 1: 0  Player 2: 0', align='center', font=('Courier', 24, 'bold'))
+
+        menu1.clear()
+        menu2.clear()
+
+        p = True
+    
+    else if (p = True):
+        return
+
 # Keyboard Binding
+
+win.onkeyrelease(gamestart, 'space')
+
 win.listen()
 
 win.onkeypress(pad1_up, 'w')
@@ -81,7 +139,7 @@ win.onkeypress(pad1_dwn, 's')
 win.onkeypress(pad2_up, 'i')
 win.onkeypress(pad2_dwn, 'k')
 
-#Game Loop
+# Game Loop
 while True:
     win.update()
 
@@ -105,15 +163,15 @@ while True:
         ball.dx *= -1
         score1 += 1
         winsound.PlaySound("offscreen.wav", winsound.SND_ASYNC)
-        pen.clear()
-        pen.write('Player 1: {}  Player 2: {}'.format(score1, score2), align='center', font=('Courier', 24, 'bold'))
+        score.clear()
+        score.write('Player 1: {}  Player 2: {}'.format(score1, score2), align='center', font=('Courier', 24, 'bold'))
     if ball.xcor() < -480:
         ball.goto(0,0)
         ball.dx *= -1
         score2 += 1
         winsound.PlaySound("offscreen.wav", winsound.SND_ASYNC)
-        pen.clear()
-        pen.write('Player 1: {}  Player 2: {}'.format(score1, score2), align='center', font=('Courier', 24, 'bold'))
+        score.clear()
+        score.write('Player 1: {}  Player 2: {}'.format(score1, score2), align='center', font=('Courier', 24, 'bold'))
 
     # Paddle Border Check
     if pad1.ycor() > 240:
@@ -128,7 +186,7 @@ while True:
     if pad2.ycor() < -240:
         pad2.sety(-240)
 
-    #Paddle & Ball Collision
+    # Paddle & Ball Collision
     if (ball.xcor() > 380 and ball.xcor() < 410) and (ball.ycor() < pad2.ycor() + 40 and ball.ycor() > pad2.ycor() -40):
         ball.setx(380)
         ball.dx *= -1
@@ -139,27 +197,25 @@ while True:
         ball.dx *= -1
         winsound.PlaySound("paddle.wav", winsound.SND_ASYNC)
 
-    #Score Detector
+    # Score Detector
     if (score2 == 7):
-        pen.clear()
-        pen.goto(0,-40)
+        score.clear()
+        score.goto(0,-40)
         pad1.goto(0,-500)
         pad2.goto(0,500)
         ball.goto(0,100)
         ball.color('black')
         ball.dx = 0
         ball.dy = 0
-        pen.write('Player 2 Wins!', align='center', font=('Courier', 72, 'bold'))
+        score.write('Player 2 Wins!', align='center', font=('Courier', 72, 'bold'))
 
     if (score1 == 7):
-        pen.clear()
-        pen.goto(0,-40)
+        score.clear()
+        score.goto(0,-40)
         pad1.goto(0,-500)
         pad2.goto(0,500)
         ball.goto(0,100)
         ball.color('black')
         ball.dx = 0
         ball.dy = 0
-        pen.write('Player 2 Wins!', align='center', font=('Courier', 72, 'bold'))
-
-
+        score.write('Player 2 Wins!', align='center', font=('Courier', 72, 'bold'))
